bd6f8f08cc4de4bc6699593421fe2835de61da17
mcm600 <mcm600@ISE-L1-1851.engr.msstate.edu>
2016-10-12 01:12:08 -0500

    Read of nodes and edges

--- Tasks/reading.py
+++ Tasks/reading.py
@@ -1,19 +1,26 @@
 import networkx as nx
-G=nx.Graph()
+G=nx.DiGraph()
 filename='C:\\Users\\mcm600\\workspace\\testing\\SanBernadinotest.gml'
 #filename='C:\\Users\\mcm600\\git\\fireopt\\data\\SanBernardino.gml'
 isnode=0
 nodename=-1
+isedge=0
+edgesource=-1
+edgetarget=-1
 
 def process(line):
     global isnode
     global nodename
+    global isedge,edgesource,edgetarget
     if line=='  node [':
         isnode=1
         return
     if line=='  ]':
         isnode=0
         nodename=-1
+        isedge=0
+        edgesource=-1
+        edgetarget=-1
         return
     if isnode==1:
         elements=line.strip().split(' ')
@@ -23,15 +30,29 @@ def process(line):
         return
     if isnode==2:
         elements=line.strip().split(' ')
-        G.node[nodename][elements[0]]=elements[1]
-        print(elements)
-
-    
-#    print(elements[1]) 
-#    print(line)
+        G.node[nodename][elements[0]]=elements[1].strip('\"')
+        return
+    if line=='  edge [':
+        isedge=1
+        return
+    if isedge==1:
+        elements=line.strip().split(' ')
+        edgesource=int(elements[1])
+        isedge=2
+        return
+    if isedge==2:
+        elements=line.strip().split(' ')
+        edgetarget=int(elements[1])
+        isedge=3
+        return
+    if isedge==3:
+        elements=line.strip().split(' ')
+        G.add_edge(edgesource,edgetarget,weight=float(elements[1]))
+        return
 
 with open(filename) as f:
     for line in f:
         process(line.splitlines()[0])
 
 print(G.nodes(data=True))
+print(G.edges(data=True))
