828f99a0d7b635bf703bfcef4ccb6363521859a1
bhuiyantanveer <metanveer29@gmail.com>
2017-01-24 16:28:01 -0600

    Update optmodel.py
    Implemented the _init_ method and the documentation

--- src/opt/optmodel.py
+++ src/opt/optmodel.py
@@ -9,39 +9,51 @@ class OptimizationModel(object):
     '''
     classdocs
     '''
-
-
-    def __init__(self, graph, paramsDict):
-        '''
-        Constructor
-        '''
-        self.createModel()
+    
+    '''
+    Constructor
+    '''
+    def __init__(self, graph, paramDF, Budget_param):
+        Fire_model = self.createModel()
+        Prob = self.ProbDecisionState(paramDF)
+        SecondStgValue = self.CalcSecondStageValue()
+        self.Solve(Fire_model, Budget_param, Prob, SecondStgValue)
+        
     
     def createModel(self):
         pass
         # create the model
-    def ProbDecisionState(self, paramsDict):
-        '''
-        The input is the data of financial assistance offereed to the landowner's and the corresponding decision
+        
+    '''
+    ProbDecisionState method:
+        The input is a data frame that contains the financial assistance offered to the landowner's and the corresponding decision
         Calculate the probabilities of a landowner's decision state for a given amount of financial assistance
         The output is the probability of a landowner to accept a cost-share plan for a given amount of financial assistance
-        '''
+    '''
+    def ProbDecisionState(self, paramDF):
+        
         return ProbDict
     
-    def CalcSecondStageValue(self):
-        '''
+    '''
+    CalcSecondStageValue method:
          Calculate the second stage objective value
         The output is the second stage objective value for each scenario 
          
-        ''' 
+    ''' 
+    
+    def CalcSecondStageValue(self):
+        
         return SecondStageValue 
     
-    def SolveFirststageModel(self, createModel, paramsDict, ProbDecisionState, CalcSecondStageValue):
-        
-        '''
+    '''
+    Solve method:
         As input takes the first stage model, total budget of the decision maker, probabilities of the decision state, and second stage Value
         for each scenario.
         solve the 1st stage model and 
         returns the optimal solution (level of financial assistance offered) and the optimal solution (expected total acreage burned)
-        '''
+    '''
+    
+    def Solve(self, createModel, Budget_param, ProbDecisionState, CalcSecondStageValue):
+        
+        
         return ObjVal, optSol
