c7b3feec5038507561aba498612c74437fb85e3e
mcm600 <mcm600@ISE-L1-1851.engr.msstate.edu>
2017-06-09 15:01:01 -0400

    Testing and results

--- src/opt/fireopt.py
+++ src/opt/fireopt.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     optModel = opt.OptimizationModel(graph, paramsDict)
 #    optModel.optimize()
     print "We've made it this far!"
-    optModel.writeResults('results.txt')
+    optModel.writeResults('results2.txt')
     print "The file has been created."
