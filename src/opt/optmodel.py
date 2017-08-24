1b6f00ad94363ff8dc20f70950ef26af6c82668c
bhuiyantanveer <metanveer29@gmail.com>
2017-08-23 22:58:05 -0500

    Updated the optModel.py

--- src/opt/optmodel.py
+++ src/opt/optmodel.py
@@ -12,11 +12,12 @@ import random
 import time
 from Data_SantaFe import *
 
+
 class OptimizationModel():
     '''
     classdocs
     '''
-    
+
     '''
     Constructor
     '''
@@ -26,6 +27,7 @@ class OptimizationModel():
         self.Budget_param = paramDF['budget']
         self.numberOfFinancialAsstValues = paramDF['numFinancialAssLevel']
         self.landowners, self.ownerNums, self.nOwners = self.createLandownersList(graph)
+        print "ownerNums", self.ownerNums
         self.LandOwnerNodeList = self.LandOwnerNodeList()
         print "line 29"
         self.Decision_states = self.CreateScenarioDecisionStates()
@@ -51,11 +53,11 @@ class OptimizationModel():
                 for l in (0,1):
                     for k in range(self.numberOfFinancialAsstValues):
                         print "[%s,%s,%s,%s] = %s" % (n,r,l,k,self.Prob[n,r,l,k])
-        
+
     def setParams(self, graph, paramDF):
         self.graph = graph
         self.paramDF = paramDF
-        
+
     def createLandownersList(self, graph):
         #iterate through nodes in graph and return a list of all the unique landowners
         landowners = {}
@@ -65,14 +67,14 @@ class OptimizationModel():
         for nodeNum,nodeAttr in list(graph.nodes(data=True)):
             if nodeAttr['owner'] not in ownerNames:
                 ownerNames.append(nodeAttr['owner'])
-        
+
         ownerNameNum = zip(ownerNames, range(len(ownerNames)))
-        
+
         for name, number in ownerNameNum:
             landowners[name] = int(number)
             ownerNums.append(number)
             nOwners = nOwners+1
-        
+
         return landowners, ownerNums, nOwners
 
     def filterProbDict(self):
@@ -87,57 +89,57 @@ class OptimizationModel():
                             if self.Prob[n,j,l,k] > -99:
                                 print "This worked."
                                 DecisionProb[n,j,k] = self.Prob[n,j,l,k]
-        print DecisionProb                    
+        print DecisionProb
         return DecisionProb
-    
 
-    def lDecState(self, n, r):  
+
+    def lDecState(self, n, r):
         l = 0
-        
+
         if self.Prob[n, r, l, 0] == -99:
             l = 1
-        
+
         return l
-    ''' 
+    '''
     def kLevel(self, r, n):
-        
+
         kLevel = []
-        
+
         for k in self.numberOfFinancialAsstValues:
             if self.Prob(n, r, lDecState(r,n),k) > 0.00001:
                 kLevel.append(k)
-        
+
         return kLevel
     '''
     def createModel(self):
         w = {}
         self.y = {}
-        
+
         m = Model("Optimization Model")
-        
+
         #Create variables
         for n in range(self.nScenario):
             for k in range(self.numberOfFinancialAsstValues):
                 for j in range(self.nOwners):
-                    w[j, k, n] = m.addVar(vtype=GRB.CONTINUOUS, name="w_j"+str(j)+"_k"+str(k)+"_n"+str(n))   
-        
+                    w[j, k, n] = m.addVar(vtype=GRB.CONTINUOUS, name="w_j"+str(j)+"_k"+str(k)+"_n"+str(n))
+
         for k in range(self.numberOfFinancialAsstValues):
             for j in range(self.nOwners):
                 self.y[j, k] = m.addVar(vtype=GRB.BINARY, name="y_j"+str(j)+"_k"+str(k))
-        
+
         m.update()
-        
+
 #        for n in (n,j,k) in self.DecisionProb:
-#            m.addConstr(quicksum(w[0,k,n] for k in (n,j,k) in self.DecisionProb) == 
+#            m.addConstr(quicksum(w[0,k,n] for k in (n,j,k) in self.DecisionProb) ==
 #                        self.SecondStgValues[n]*quicksum(self.DecisionProb[n,0,k]*self.y[0, k]
 #                        for k in (n,j,k) in self.DecisionProb), name = "6a_n"+str(n))
-        
+
         #6a updated
         for n in range(self.nScenario):
-            m.addConstr(quicksum(w[0,k,n] for k in range(self.numberOfFinancialAsstValues)) == 
+            m.addConstr(quicksum(w[0,k,n] for k in range(self.numberOfFinancialAsstValues)) ==
                         self.SecondStgValues[n]*quicksum(self.DecisionProb[n,0,k]*self.y[0, k]
                         for k in range(self.numberOfFinancialAsstValues)), name = "6a_n"+str(n))
-        
+
         #6b updated
         for r in range(1,self.nOwners):
             for n in range(self.nScenario):
@@ -153,33 +155,33 @@ class OptimizationModel():
                             for k in range(self.numberOfFinancialAsstValues)),
                             name = "6b_j"+str(r)+"_n"+str(n))
                 #m.addConstr(sum1 == sum2, name="6b_j" + str(r) + "_n" + str(n))
-        
+
         #6c updated
         for k in range(self.numberOfFinancialAsstValues):
             for r in range(self.nOwners):
                 for n in range(self.nScenario):
                     m.addConstr(w[r, k, n] <= self.y[r, k]*self.SecondStgValues[n],
                                 name = "6c_j"+str(r)+"_k"+str(k)+"_n"+str(n))
-        
+
         #6e
         m.addConstr(quicksum(quicksum(self.C_k[k]*self.y[j, k] for k in range(self.numberOfFinancialAsstValues))
                             for j in range(self.nOwners)) <= self.Budget_param, name = "6e")
-        
+
         #6f
         for j in range(self.nOwners):
             m.addConstr(quicksum(self.y[j, k] for k in range(self.numberOfFinancialAsstValues)) == 1,
                         name = "6f_j"+str(j))
-                                              
+
         #6a -- Objective
         m.setObjective(quicksum(quicksum(w[self.nOwners-1, k, n] for k in range(self.numberOfFinancialAsstValues))
-                                for n in range(self.nScenario)), GRB.MINIMIZE)        
+                                for n in range(self.nScenario)), GRB.MINIMIZE)
 
         m.update()
-        
+
         m.optimize()
-        
+
         return m
-        
+
     '''
     ProbDecisionState method:
         The input is a data frame that contains the financial assistance offered to the landowner's and the corresponding decision
@@ -192,9 +194,9 @@ class OptimizationModel():
         Decision_states = {}
         for s in range(self.nScenario):
             Decision_states[s] = [int(l[s][j]) for j in range(len(self.ownerNums))]
-        
+
         return Decision_states
-    
+
     def CreateLogRegDataFile(self):  ##newly added method
 
         Financial_level_data = "../../data/AsstanceLevel_probability.csv"
@@ -214,44 +216,45 @@ class OptimizationModel():
                                                                                Financial_level_df['Probability'][k]])
 
         return LogRegression_df
-    
-    
+
+
     def ProbDecisionState(self, paramDF):
         #Data_file = "../../data/LogRegression.csv"
         #Data_file = "../../data/LogRegression_3.csv"
-        
+
         Data_df = self.LogitRegData
         #Data_df = pd.read_csv(Data_file, delimiter=',', usecols=[0, 1, 2, 3])
-        
+
         nOwner = (len(self.ownerNums))
         nDecision_state = 2
         #nLevel = 5
-        
+        #print "Data_df", Data_df
         train_Feature = Data_df['Level'].as_matrix().reshape(-1, 1)  # we only take the first two features.
         train_Target = Data_df['Decision'].as_matrix()
-        
+
+
         h = .02  # step size in the mesh
-        
+
         logreg = linear_model.LogisticRegression(C=1e5)
-        
+
         # we create an instance of Neighbours Classifier and fit the data.
-        logreg.fit(train_Feature, train_Target)
-        
+        logreg.fit(train_Feature, train_Target.astype(str))
+
         #np.set_printoptions(threshold=1000000)
-        
+
         State_prob = logreg.predict_proba(train_Feature)
         print State_prob
-        
+
         # Put the result into a dictionary
         ProbDict = {}
-        
+
         for s in range(self.nScenario):
             for i in range(len(self.ownerNums)):
                 for j in range(nDecision_state):
                     for k in range(self.numberOfFinancialAsstValues):
-                        owner = Data_df[Data_df['Level'] == k].iloc[0]['Owner']
+                        owner = Data_df[Data_df['Level'] == k].iloc[0]['OwnerIdx']
                         ProbDict[s, i, j, k] = State_prob[owner,j]
-        
+
         for s in range(self.nScenario):
             for i in range(len(self.ownerNums)):
                 for k in range(self.numberOfFinancialAsstValues):
@@ -259,137 +262,136 @@ class OptimizationModel():
                     if self.Decision_states[s][i]==0:
                         l=1
                     ProbDict[s, i, l, k]= -99
-                   
+
         return ProbDict
-    
+
     '''
     CalcSecondStageValue method:
          Calculate the second stage objective value
-        The output is the second stage objective value for each scenario 
-         
-    ''' 
+        The output is the second stage objective value for each scenario
+
+    '''
     def LandOwnerNodeList(self):
-        Graph = nx.read_gml('../../data/SantaFe.gml')
+        Graph = nx.read_gml('../../data/SantaFe with 3 landowners.gml')
         Landowners_node_lst = {}
         for i in range(len(self.ownerNums)):
-            Landowners_node_lst[i] = [] 
-        
+            Landowners_node_lst[i] = []
+
         for node in Graph.nodes(data=True):
         #print "node", node[0]
-            for i in range(len(self.ownerNums)): 
+            for i in range(len(self.ownerNums)):
                 if int(node[1]['owner']) == i+1: ##checking the owner of a node in data.gml is the owner in the list
-                    Landowners_node_lst[i].append(node[0]) 
-                    
+                    Landowners_node_lst[i].append(node[0])
+
         return Landowners_node_lst
-    
+
     def spread(self, ignition_points, Land, SPLength, fire_duration):
 
         SumBurnt = 0
         Burnt_WUI = 0
-    
+
         for nod in Land.nodes():
             min_arrival_time = Duration[0] + 0.00001
             for igpoint in ignition_points:
-    
-    
+
+
                 if SPLength[igpoint][nod] < min_arrival_time:
                     min_arrival_time = SPLength[igpoint][nod]
                     #Land.node[nod]['fire'] = igpoints.index(igpoint)+1
                     Land.node[nod]['is_it_burned'] = 1
-    
+
             Land.node[nod]['fire_arrival_time'] = min_arrival_time
-    
-    
+
         for nod in Land.nodes():
             if Land.node[nod]['fire_arrival_time'] <= fire_duration:
                 Land.node[nod]['is_it_burned'] = 1
-    
+
                 SumBurnt += ValueAtRisk[nod]
-    
+
                 if Land.node[nod]['fire'] == 1:
                     Land.node[nod]['color'] = 'r'   ###what's the use of different color-illustration
-    
+
                 if Land.node[nod]['fire'] == 2:
                     Land.node[nod]['color'] = 'y'
-    
+
                 if Land.node[nod]['fire'] == 3:
                     Land.node[nod]['color'] = 'orange'
-    
+
                 if Land.node[nod]['fire'] == 4:
                     Land.node[nod]['color'] = 'pink'
-    
+
                 if Land.node[nod]['fire'] == 5:
                     Land.node[nod]['color'] = 'white'
-    
+
                 if Land.node[nod]['WUI'] == 1:
                     Burnt_WUI += 1
-    
+
         for i in range(0,len(ignition_points)):
             Land.node[ignition_points[i]]['color'] = 'b'
-    
+
         return (SumBurnt, Burnt_WUI)
-    
+
     def Generate_Random_igpoint(self, Num_igpoints): #randomly generate upto 5 ignition point
 
         correctOrder = False
-    
+
         while(not correctOrder):
-    
+
             nodes = random.sample(range(0, N_Nodes-1), Num_igpoints)
-    
+
             if Num_igpoints == 2:
-    
+
                 nod1 = nodes[0]
                 nod2 = nodes[1]
-    
-    
+
+
                 if (nod1 < nod2):
                     correctOrder = True
-    
+
             if Num_igpoints == 3:
-    
+
                 nod1 = nodes[0]
                 nod2 = nodes[1]
                 nod3 = nodes[2]
-    
+
                 if (nod1 < nod2 < nod3):
                     correctOrder = True
-    
+
             if Num_igpoints == 4:
-    
+
                 nod1 = nodes[0]
                 nod2 = nodes[1]
                 nod3 = nodes[2]
                 nod4 = nodes[3]
-    
+
                 if (nod1 < nod2 < nod3 < nod4):
                     correctOrder = True
-    
+
             if Num_igpoints == 5:
-    
+
                 nod1 = nodes[0]
                 nod2 = nodes[1]
                 nod3 = nodes[2]
                 nod4 = nodes[3]
                 nod5 = nodes[4]
-    
+
                 if (nod1 < nod2 < nod3 < nod4 < nod5):
                     correctOrder = True
-    
+
         return(nodes)
-    
+
     def SDCalculate(self, Burnt,Number_scenario_counted,which):
 
         SumBurnt = sum([i[which] for i in Burnt])
         AverageBurnt = float(SumBurnt)/Number_scenario_counted
-    
+
         SumSquare_deviation_of_mean = sum([(i[which] - AverageBurnt)**2 for i in Burnt])
-    
+
         sdBurnt = (float(SumSquare_deviation_of_mean)/Number_scenario_counted)**0.5
-    
+
         return(AverageBurnt, sdBurnt)
-    
-    
+
+
     def CalcAllSecondStageValues(self):
         secondStageValues = {}
         #secondStageValues[s] = self.CalcSecondStageValue()
@@ -407,22 +409,22 @@ class OptimizationModel():
                     Sum_WUI += 1
                 else:
                     Land.node[nod]['WUI'] = 0
-            
+
             fire_duration = Duration[0]
-            MaxDuration = max(Duration) + 100 ## Duration comes from where...why add 100?? 
-    
+            MaxDuration = max(Duration) + 100 ## Duration comes from where...why add 100??
+
             t = [[MaxDuration for i in range(N_Nodes)] for i in range(N_Nodes)]  ## where from N_nodes comes-the data
-    
+
             for r in range(N_Nodes):
-    
+
                 for q in Neighbor[r]:  ##where the neighbors of the nodes are defined
-        
+
                     distance_q_r = DGG[q][r]['weight']
-                    t[q][r] = distance_q_r/ROS[q][r] 
-                    
+                    t[q][r] = distance_q_r/ROS[q][r]
+
                     'Distance between neighbor nodes is set'
                     'to the weight of the graph, so we can use weight instead'
-    
+
             ###modify the time distance for the treated cells
             for Lowner in range(len(self.Decision_states[s])):
                 if self.Decision_states[s][Lowner]==1:
@@ -430,27 +432,27 @@ class OptimizationModel():
                         for q in Neighbor[r]:
                             if q in self.LandOwnerNodeList[Lowner]:
                                 t[q][r]= 100000*60
-            
+
             '#  Rebuilding the graph with time attribute'
             Landscape = DGG.copy() ##got the landscape with time as the distance
-            
+
             for link in DGG.edges():
                 Landscape.add_edge(link[0],link[1], Time_Distance = t[link[0]][link[1]])
-            
+
             SPLength = nx.all_pairs_dijkstra_path_length(Landscape, weight = 'Time_Distance')
             Number_Sub_scenario_counted = 0
             SDBurnt = 100
-            
+
             Num_igpoints = 5
             Max_Num__sub_Scenario = 5000
-            
-            nodes = [] 
-            
+
+            nodes = []
+
             Burnt = []
             #SumBurnt = 0
             Total_Burnt= 0
             AveBurnt = 0
-            
+
             while (Number_Sub_scenario_counted < Max_Num__sub_Scenario):
                 nodes.append(self.Generate_Random_igpoint(Num_igpoints))
                 igpoints = nodes[Number_Sub_scenario_counted][:Num_igpoints]
@@ -459,7 +461,7 @@ class OptimizationModel():
                 (AveBurnt, SDBurnt) = self.SDCalculate(Burnt,Number_Sub_scenario_counted,0)
             Total_Burnt = sum([i[0] for i in Burnt])
             (AveWUIBurnt,STDWUIBurnt) = self.SDCalculate(Burnt,Number_Sub_scenario_counted,1)
-            secondStageValues[s] = AveBurnt    
+            secondStageValues[s] = AveBurnt
             print "Scenario %s" % (s)
         # fill in here
         return secondStageValues
